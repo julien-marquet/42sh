70ee7849ff64afe8d095904e95de706a2e118138
Julien Marquet <jmarquet@z2r1p5.le-101.fr>
2019-04-08 02:12:08 +0200

    shorten some whiles

--- srcs/builtins/builtins_storage/builtin_unset.c
+++ srcs/builtins/builtins_storage/builtin_unset.c
@@ -6,7 +6,7 @@
 /*   By: jmarquet <jmarquet@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/04/07 23:23:29 by jmarquet     #+#   ##    ##    #+#       */
-/*   Updated: 2019/04/08 01:20:26 by jmarquet    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/04/08 02:10:24 by jmarquet    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -20,9 +20,6 @@ int		builtin_unset(t_sh_state *sh_state, int ac, const char **av, int fd_out)
 	fd_out++;
 	i = 1;
 	while (i < ac)
-	{
-		remove_entry_storage(&sh_state->internal_storage, av[i]);
-		i++;
-	}
+		remove_entry_storage(&sh_state->internal_storage, av[i++]);
 	return (0);
 }
