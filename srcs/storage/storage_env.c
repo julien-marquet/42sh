2bd64b3bdda5f71164f281c2c16fafdb1974c2d5
Julien Marquet <jmarquet@z2r5p6.le-101.fr>
2019-04-08 19:48:21 +0200

    Env array generator

--- srcs/storage/storage_env.c
+++ srcs/storage/storage_env.c
@@ -6,7 +6,7 @@
 /*   By: jmarquet <jmarquet@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/04/05 17:25:57 by jmarquet     #+#   ##    ##    #+#       */
-/*   Updated: 2019/04/08 01:58:04 by jmarquet    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/04/08 19:48:07 by jmarquet    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -45,4 +45,49 @@ void	print_env(t_list *internal_storage, int fd)
 			ft_putendl_fd(((t_internal_storage *)(tmp->content))->string, fd);
 		tmp = tmp->next;
 	}
+}
+
+static int	fill_arr_env(char **arr_env, t_list *storage)
+{
+	t_list	*tmp;
+	size_t	i;
+
+	tmp = storage;
+	i = 0;
+	while (tmp != NULL)
+	{
+		if (((t_internal_storage *)tmp->content)->exported == 1 ||
+	((t_internal_storage *)tmp->content)->new_entry == 1)
+		{
+			if ((arr_env[i] = ft_strdup((const char *)(
+		(t_internal_storage *)tmp->content)->string)) == NULL)
+				return (1);
+			i++;
+		}
+		tmp = tmp->next;
+	}
+	arr_env[i] = NULL;
+	return (0);
+}
+
+char	**generate_env(t_list *storage)
+{
+	t_list	*tmp;
+	size_t	arr_len;
+	char	**arr_env;
+
+	tmp = storage;
+	arr_len = 0;
+	while (tmp != NULL)
+	{
+		if (((t_internal_storage *)tmp->content)->exported == 1 ||
+	((t_internal_storage *)tmp->content)->new_entry == 1)
+			arr_len++;
+		tmp = tmp->next;
+	}
+	if ((arr_env = (char **)ft_memalloc(sizeof(char *) * (arr_len + 1))) == NULL)
+		return (NULL);
+	if (fill_arr_env(arr_env, storage) == 1)
+		return (NULL);
+	return (arr_env);
 }
