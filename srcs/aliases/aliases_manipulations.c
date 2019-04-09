38527692d706a85a1e1c588a6d7336a7651af622
Julien Marquet <jmarquet@z2r5p6.le-101.fr>
2019-04-09 02:10:16 +0200

    Better add_allias

--- srcs/aliases/aliases_manipulations.c
+++ srcs/aliases/aliases_manipulations.c
@@ -6,7 +6,7 @@
 /*   By: jmarquet <jmarquet@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/04/08 20:18:26 by jmarquet     #+#   ##    ##    #+#       */
-/*   Updated: 2019/04/09 01:51:10 by jmarquet    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/04/09 02:09:11 by jmarquet    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -18,38 +18,27 @@ int		add_alias(t_list **aliases, const char *name, const char *value)
 	t_list		*node;
 	size_t		len;
 	char		*entry;
-	char		*new_value;
 
-	len = 0;
 	if (name == NULL)
 		return (1);
-	if (value)
-	{
-		len = ft_strlen(value) + 2;
-		if ((new_value = ft_memalloc(len + 1)) == NULL)
-			return (1);
-		new_value[0] = '\'';
-		ft_strcpy(&new_value[1], value);
-		new_value[len - 1] = '\'';
-	}
+	len = value ? ft_strlen(value) : 0;
 	len += ft_strlen(name) + 1;
 	if (ft_strlen(name) == 0)
 		return (0);
 	if ((node = find_alias_by_name(*aliases, name)) != NULL)
 	{
-		if (update_existing_alias(node, name, new_value, len) == 1)
+		if (update_existing_alias(node, name, value, len) == 1)
 			return (1);
 	}
 	else
 	{
-		if ((entry = merge_name_value(name, new_value, len)) == NULL)
+		if ((entry = merge_name_value(name, value, len)) == NULL)
 			return (1);
 		if ((node = ft_lstnew((const void *)entry, len + 1)) == NULL)
 			return (1);
 		free(entry);
 		ft_lstprepend(aliases, node);
 	}
-	free(new_value);
 	return (0);
 }
 
