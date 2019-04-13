eaa9cdfdae20da09e003e781704e4d573b186a64
legrivel <legrivel@student.le-101.fr>
2019-04-13 19:32:34 +0200

    Merge

--- incl/builtins/builtins_dispatcher.h
+++ incl/builtins/builtins_dispatcher.h
@@ -22,6 +22,7 @@
 # include "builtins/builtin_exit.h"
 # include "builtins/builtin_echo/builtin_echo.h"
 # include "builtins/builtin_test/builtin_test.h"
+# include "builtins/builtins_hash/builtin_hash.h"
 
 int		builtins_dispatcher(t_sh_state *sh_state,
 		const char **av, t_builtin_context *context, int background);
