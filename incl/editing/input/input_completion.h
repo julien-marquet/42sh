58ab4f347d8ddfc065e74885a54f6cd6e647580e
legrivel <legrivel@student.le-101.fr>
2019-04-23 19:22:49 +0200

    Standards

--- incl/editing/input/input_completion.h
+++ incl/editing/input/input_completion.h
@@ -4,5 +4,10 @@
 # include <dirent.h>
 # include "common.h"
 
+char	*trim(char *str);
+size_t	is_dir(char *path);
+size_t	is_stopping(char str);
+size_t	is_closed(char *str, char *base);
+
 int		handle_completion(t_input_data *input, t_sh_state *sh_state);
 #endif
--- srcs/editing/input/completion/completion.c
+++ srcs/editing/input/completion/completion.c
@@ -2,62 +2,6 @@
 #include "editing/input/input_action_handlers.h"
 #include "parse/parse.h"
 
-static char	*trim(char *str)
-{
-	char	*trimmed;
-
-	while (*str == ' ')
-		str += 1;
-	trimmed = str;
-	while (*str != ' ' && *str != '\0')
-		str += 1;
-	*str = '\0';
-	return (trimmed);
-}
-
-static int	is_reversed_closed(char *str, char *base)
-{
-	char	c;
-	size_t	closed;
-
-	c = *str;
-	closed = 0;
-	str -= 1;
-	while (str != base)
-	{
-		if (*str == c && *(str - 1) != '\\')
-			closed = !closed;
-		str -= 1;
-	}
-	return (closed);
-}
-
-static int	is_closed(char *str, char *base)
-{
-	size_t	quoting;
-
-	if (is_reversed_closed(str, base))
-		return (1);
-	if (*str == '\'')
-		quoting = 1;
-	else if (*str == '"')
-		quoting = 2;
-	else
-		quoting = 3;
-	str += 1;
-	while (*str)
-	{
-		if (quoting == 1 && *str == '\'' && *(str - 1) != '\\')
-			return (1);
-		if (quoting == 2 && *str == '"' && *(str - 1) != '\\')
-			return (1);
-		if (quoting == 3 && *str == '`' && *(str - 1) != '\\')
-			return (1);
-		str += 1;
-	}
-	return (0);
-}
-
 static char	*handle_quotes(char *word)
 {
 	char	*tmp;
@@ -127,18 +71,6 @@ static char	*handle_expand(char *word, t_sh_state *sh_state)
 	else
 		free(word);
 	return (handle_quotes(currentWord));
-}
-
-static int	is_stopping(char str)
-{
-	return (str == ' ' ||
-		str == ';' ||
-		str == '|' ||
-		str == '&' ||
-		str == '>' ||
-		str == '<' ||
-		str == ':' ||
-		str == '+');
 }
 
 static char	*get_current_word(t_input_data *input, t_sh_state *sh_state)
@@ -237,15 +169,6 @@ static char	*get_path(t_input_data *input, size_t get_all)
 		pointer += 1;
 	}
 	return (tmp);
-}
-
-static size_t	is_dir(char *path)
-{
-	struct stat	stats;
-
-	if (stat(path, &stats) == -1)
-		return (0);
-	return (S_ISDIR(stats.st_mode));
 }
 
 // TODO free `completed` on error
