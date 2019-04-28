192064b1d7af91800de74bbcbeb65dfcf84fe077
legrivel <legrivel@student.le-101.fr>
2019-04-28 21:12:42 +0200

    Added an error for exit builtin

--- srcs/builtins/builtin_exit.c
+++ srcs/builtins/builtin_exit.c
@@ -11,15 +11,40 @@
 /*                                                        /                   */
 /* ************************************************************************** */
 
+#include "error_handler.h"
 #include "builtins/builtin_exit.h"
 #pragma GCC diagnostic ignored "-Wunused-parameter"
+
+static int	check_error(char *arg)
+{
+	char	*tmp;
+	char	*pointer;
+
+	pointer = arg;
+	while (*pointer)
+	{
+		if (*pointer < 48 || *pointer > 57)
+		{
+			if ((tmp =
+		ft_construct_str(6, NAME, ": ", "exit", ": ", arg, ": ")) == NULL)
+				return (-1);
+			print_error(tmp, "numeric argument required", 2);
+			free(tmp);
+			return (-1);
+		}
+		pointer += 1;
+	}
+	return (0);
+}
 
 int		builtin_exit(t_sh_state *sh_state, int ac, const char **av,
 t_builtin_context *context)
 {
+	if (check_error((char *)(*(av + 1))) == -1)
+		return (1);
 	if (ac > 2)
 	{
-		write(2, "exit:, Too many arguments.\n", 29);
+		write(2, "exit: too many arguments\n", 25);
 		return (1);
 	}
 	sh_state->exit_sig = 1;
