7d57c677c595f9bc243797f35d3791d57a6392bc
Julien Marquet <julienmarquet9@gmail.com>
2019-04-29 15:33:08 +0200

    echo now return 1 on write error

--- srcs/builtins/builtin_echo/builtin_echo.c
+++ srcs/builtins/builtin_echo/builtin_echo.c
@@ -6,7 +6,7 @@
 /*   By: jmarquet <jmarquet@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/04/07 22:59:24 by jmarquet     #+#   ##    ##    #+#       */
-/*   Updated: 2019/04/28 05:55:45 by jmarquet    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/04/29 15:31:47 by jmarquet    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -14,7 +14,7 @@
 #include "builtins/builtin_echo/builtin_echo.h"
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 
-static int	print_octal(const char *str)
+static int	print_octal(const char *str, int *err)
 {
 	int				res;
 	unsigned char	to_print;
@@ -29,7 +29,8 @@ static int	print_octal(const char *str)
 	res = ft_atoi(new ? new : str);
 	res = convert_to_decimal(res);
 	to_print = (unsigned char)res;
-	write(1, &to_print, 1);
+	if (write(1, &to_print, 1) == -1)
+		*err = 1;
 	if (new)
 	{
 		free(new);
@@ -38,36 +39,36 @@ static int	print_octal(const char *str)
 	return (ret);
 }
 
-static int	print_extended(const char *str, int fd)
+static int	print_extended(const char *str, int fd, int *err)
 {
 
 	if (*str == '0')
-		return (print_octal(str));
-	else if (*str == 'a')
-		write(fd, "\a", 1);
-	else if (*str == 'b')
-		write(fd, "\b", 1);
-	else if (*str == 'f')
-		write(fd, "\f", 1);
-	else if (*str == 'n')
-		write(fd, "\n", 1);
-	else if (*str == 'r')
-		write(fd, "\r", 1);
-	else if (*str == 't')
-		write(fd, "\t", 1);
-	else if (*str == 'v')
-		write(fd, "\v", 1);
-	else if (*str == '\\')
-		write(fd, "\\", 1);
+		return (print_octal(str, err));
+	else if (*str == 'a' && write(fd, "\a", 1))
+		*err = 1;
+	else if (*str == 'b' && write(fd, "\b", 1))
+		*err = 1;
+	else if (*str == 'f' && write(fd, "\f", 1))
+		*err = 1;
+	else if (*str == 'n' && write(fd, "\n", 1))
+		*err = 1;
+	else if (*str == 'r' && write(fd, "\r", 1))
+		*err = 1;
+	else if (*str == 't' && write(fd, "\t", 1))
+		*err = 1;
+	else if (*str == 'v' && write(fd, "\v", 1))
+		*err = 1;
+	else if (*str == '\\' && write(fd, "\\", 1))
+		*err = 1;
 	else
 	{
-		write(fd, "\\", 1);
-		write(fd, str, 1);
+		if (write(fd, "\\", 1) == -1 || write(fd, str, 1) == -1)
+			*err = 1;
 	}
 	return (1);
 }
 
-static int	handle_ext(const char *str, int fd)
+static int	handle_ext(const char *str, int fd, int *err)
 {
 	int		res;
 	int		tmp;
@@ -77,7 +78,8 @@ static int	handle_ext(const char *str, int fd)
 	{
 		if (*str != '\\')
 		{
-			write(fd, str, 1);
+			if (write(fd, str, 1) == -1)
+				*err = 1;
 			str++;
 		}
 		else
@@ -87,7 +89,7 @@ static int	handle_ext(const char *str, int fd)
 				res = 1;
 			else
 			{
-				tmp = print_extended(str, fd);
+				tmp = print_extended(str, fd, err);
 				str += tmp;
 			}
 		}
@@ -95,12 +97,14 @@ static int	handle_ext(const char *str, int fd)
 	return (res);
 }
 
-static void	printer(int i, int fd, const char **av, const char *opts)
+static int	printer(int i, int fd, const char **av, const char *opts)
 {
 	int		first;
 	int		ext_opt;
 	int		no_nl_opt;
+	int		err;
 
+	err = 0;
 	first = 1;
 	ext_opt = opts && ft_strchr(opts, 'e') != NULL;
 	no_nl_opt = opts && ft_strchr(opts, 'n') != NULL;
@@ -108,16 +112,19 @@ static void	printer(int i, int fd, const char **av, const char *opts)
 	{
 		if (first)
 			first = 0;
-		else
-			write(fd, " ", 1);
+		else if (write(fd, " ", 1) == -1)
+			return (1);
 		if (ext_opt == 1)
-			no_nl_opt |= handle_ext(av[i], fd);
-		else
-			ft_putstr_fd(av[i], fd);
+			no_nl_opt |= handle_ext(av[i], fd, &err);
+		else if (write(fd, av[i], ft_strlen(av[i])) == -1)
+			return (1);
 		i++;
 	}
+	if (err != 0)
+		return (err);
 	if (!opts || !no_nl_opt)
-		write(fd, "\n", 1);
+		return (write(fd, "\n", 1) == -1);
+	return (0);
 }
 
 int			builtin_echo(t_sh_state *sh_state, int ac,
@@ -125,18 +132,19 @@ const char **av, t_builtin_context *context)
 {
 	int		i;
 	char	*opts;
+	int		res;
 
+	res = 0;
 	add_origin(&context->origin, "echo");
 	if ((i = handle_builtin_options(av, "ne", &opts, context)) == -1)
-		return (1);
+		res = 1;
 	else if (i == 0)
 	{
-		print_error(context->origin, "usage: echo [-ne] [args ... ]",
-	2);
-		return (1);
+		print_error(context->origin, "usage: echo [-ne] [args ... ]", 2);
+		res = 1;
 	}
-	else
-		printer(i, 1, av, opts);
+	else if (printer(i, 1, av, opts) != 0)
+		res = 1;
 	ft_strdel(&opts);
-	return (0);
+	return (res);
 }
