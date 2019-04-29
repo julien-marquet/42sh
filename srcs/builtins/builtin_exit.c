198a60115a7e0f23e7893ced3621a85e7e53851b
Julien Marquet <julienmarquet9@gmail.com>
2019-04-29 09:34:42 +0200

    exit behave correctly

--- srcs/builtins/builtin_exit.c
+++ srcs/builtins/builtin_exit.c
@@ -6,50 +6,51 @@
 /*   By: jmarquet <jmarquet@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/04/07 22:38:07 by jmarquet     #+#   ##    ##    #+#       */
-/*   Updated: 2019/04/28 05:57:11 by jmarquet    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/04/29 09:33:26 by jmarquet    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-#include "error_handler.h"
 #include "builtins/builtin_exit.h"
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 
-static int	check_error(char *arg)
+int		str_is_digit(const char *av)
 {
-	char	*tmp;
-	char	*pointer;
+	int		i;
 
-	pointer = arg;
-	while (*pointer)
+	i = 0;
+	while (av[i] != '\0')
 	{
-		if (*pointer < 48 || *pointer > 57)
-		{
-			if ((tmp =
-		ft_construct_str(6, NAME, ": ", "exit", ": ", arg, ": ")) == NULL)
-				return (-1);
-			print_error(tmp, "numeric argument required", 2);
-			free(tmp);
-			return (-1);
-		}
-		pointer += 1;
+		if (!ft_isdigit(av[i]))
+			return (0);
+		i++;
 	}
-	return (0);
+	return (1);
 }
 
 int		builtin_exit(t_sh_state *sh_state, int ac, const char **av,
 t_builtin_context *context)
 {
-	if (check_error((char *)(*(av + 1))) == -1)
-		return (1);
-	if (ac > 2)
+	add_origin(&context->origin, "exit");
+	if (ac == 2)
 	{
-		write(2, "exit: too many arguments\n", 25);
+		if (str_is_digit(av[1]) == 0)
+		{
+			add_origin(&context->origin, av[1]);
+			print_error(context->origin, "numeric argument required", 2);
+			return (1);
+		}
+		sh_state->exit_sig = 1;
+		return (ft_atoi(av[1]));
+	}
+	else if (ac > 2)
+	{
+		print_error(context->origin, "too many arguments", 2);
 		return (1);
 	}
-	sh_state->exit_sig = 1;
-	if (ac > 1)
-		return (ft_atoi(av[1]));
 	else
+	{
+		sh_state->exit_sig = 1;
 		return (0);
+	}
 }
